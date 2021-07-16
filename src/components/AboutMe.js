d49396f96b24b5176d2eeeea37e52c5402b0fb79
phillipchau <pc08130@gmail.com>
2021-07-16 13:56:46 -0700

    modified about me

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -3,7 +3,7 @@ import '../App.css';
 function AboutMe() {
   return (
     <div className="AboutMe">
-        <p>Hi! My name is Phillip Chau and I'm currently a junior studying Computer Science at the University of Pennsylvania. I'm passionate about using data science and software engineering in order to reveal new insights and improve the world around me. My interests primarily lie in Machine Learning and Full Stack Development and am currently looking for Data Science and SWE internship positions.</p>
+        <p>Hi! My name is Phillip Chau and I'm currently a junior studying Computer Science at the University of Pennsylvania. I'm passionate about using data science and software engineering in order to reveal new insights and improve the world around me. My interests primarily lie in Machine Learning and Full Stack Development and I'm currently looking for Data Science and SWE internship positions.</p>
         <p>In my free time, I enjoy playing tennis, watching basketball, exploring nature and learning new technologies! </p>
     </div>
   );
